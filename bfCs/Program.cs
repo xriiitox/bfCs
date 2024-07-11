f4afd898845f4c430a29c19cd6f869a0bf82ea3b
Josh Baron <joshbaron48@outlook.com>
2024-07-11 17:13:33 -0400

    i don't know how to use antlr4

--- bfCs/Program.cs
+++ bfCs/Program.cs
@@ -1,19 +1,79 @@
-﻿using Antlr4.Runtime;
-using Antlr4.Runtime.Tree;
-
-namespace bfCs;
+﻿namespace bfCs;
 
 class Program
 {
-    public static void Main(string[] args)
+    static byte[] _byteArr = new byte[30000];
+    static int _pointer = 0;
+    static readonly string _program = File.ReadAllText("./program.bf");
+    public static void Main()
     {
-        ICharStream inputStream = CharStreams.fromPath(args[0]);
-        BrainfuckLexer brainfuckLexer = new BrainfuckLexer(inputStream);
-        CommonTokenStream commonTokenStream = new CommonTokenStream(brainfuckLexer);
-        BrainfuckParser brainfuckParser = new BrainfuckParser(commonTokenStream);
+         // store bracket indices
+        
+        RunCmd();
 
-        BrainfuckParser.File_Context fileContext = brainfuckParser.file_();
-        BrainfuckListener listener = new BrainfuckListener();
-        ParseTreeWalker.Default.Walk(listener, fileContext);
+
+    }
+
+    private static void RunCmd()
+    {
+        var bracketPairLooping = 0; // integer to allow for nested loops
+        var jumpBackIndices = new List<int>();
+
+
+        for (int i = 0; i < _program.Length; i++)
+        {
+            if (bracketPairLooping == 0)
+            {
+                switch (_program[i])
+                {
+                    case '>':
+                        _pointer = _pointer == 29999 ? 0 : _pointer + 1;
+                        break;
+                    case '<':
+                        _pointer = _pointer == 0 ? 29999 : _pointer - 1;
+                        break;
+                    case '+':
+                        _byteArr[_pointer]++; // wrapping is implemented in byte already
+                        break;
+                    case '-':
+                        _byteArr[_pointer]--;
+                        break;
+                    case '.':
+                        Console.Write(char.ConvertFromUtf32(_byteArr[_pointer]));
+                        break;
+                    case ',':
+                        _byteArr[_pointer] = Convert.ToByte(Console.ReadLine());
+                        break;
+                    case '[':
+                        bracketPairLooping++;
+                        jumpBackIndices.Add(i);
+                        break;
+                    case ']':
+                        if (_byteArr[_pointer] != 0)
+                        {
+                            i = jumpBackIndices.Last();
+                        }
+
+                        break;
+                    default:
+                        continue;
+                }
+            }
+            else
+            {
+                if (_program[i] == '[')
+                {
+                    bracketPairLooping++;
+                    jumpBackIndices.Add(i);
+                    continue;
+                }
+
+                if (_program[i] == ']')
+                {
+                    bracketPairLooping--;
+                    i = jumpBackIndices.Last() + 1;
+                }
+            }
+        }
     }
 }
